0edda21163c2545de41474abcba49d1076164fd9
mgpocky <kkm1447@naver.com>
2020-11-29 11:18:26 +0900

    Add Port

--- src/01/YoutubeVideo.jsx
+++ src/01/YoutubeVideo.jsx
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class YoutubeVideo extends Component {
   render() {
     return (
       <div>
-        <object type="text/html" width="100%" height="500" data={this.props.link} />
+        <object
+          type="text/html"
+          width="100%"
+          height="500"
+          alt={this.props.data}
+          data={this.props.link}
+        />
       </div>
     );
   }
